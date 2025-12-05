29a78b580a1b0635aef9ad08cd10097485318e69
Jonathan Hook <jonathan.hook@york.ac.uk>
2025-12-05 07:46:16 +0000

    Part 1 complete

---
+++ day_5/d5_part_1.py
@@ -0,0 +1,49 @@
+from aocd import get_data
+
+
+def read_file(filename: str) -> str:
+    with open(filename) as f:
+        return f.read()
+
+
+def parse_input(data: str) -> tuple[set[tuple[int, int]], list[int]]:
+    inputParts = data.split("\n\n")
+
+    fresh = set()
+    for l in inputParts[0].splitlines():
+        part = l.split("-")
+        fresh.add((int(part[0]), int(part[1])))
+
+    available = []
+    for l in inputParts[1].splitlines():
+        available.append(int(l))
+
+    return (fresh, available)
+
+
+def in_ranges(item: int, ranges: set[tuple[int, int]]) -> bool:
+    for r in ranges:
+        if item >= r[0] and item <= r[1]:
+            return True
+    return False
+
+
+def task(data: str) -> int:
+    fresh, available = parse_input(data)
+
+    count = 0
+    for a in available:
+        if in_ranges(a, fresh):
+            count += 1
+
+    return count
+
+
+def test_example() -> None:
+    assert task(read_file("day_5/test.txt")) == 3
+
+
+def test_real() -> None:
+    result = task(get_data(day=5, year=2025))
+    print(result)
+    assert result == 517
