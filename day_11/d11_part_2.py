7f3afde82352d339f49d0f538d3f0d8622d43153
Jonathan Hook <jonathan.hook@york.ac.uk>
2025-12-11 21:20:43 +0000

    Working but too slow

---
+++ day_11/d11_part_2.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+from aocd import get_data
+
+
+class Node:
+    def __init__(self, id: str) -> None:
+        self.id = id
+        self.connections: list[Node] = []
+
+    def connect(self, n: Node) -> None:
+        self.connections.append(n)
+
+
+def read_file(filename: str) -> str:
+    with open(filename) as f:
+        return f.read()
+
+
+def parse_input(data: str) -> list:
+    nodes = []
+    for line in data.splitlines():
+        parts = line.split(" ")
+        id = parts[0][:-1]
+        connections = parts[1:]
+        nodes.append((id, connections))
+
+    return nodes
+
+
+def build_graph(nodeData: list) -> Node:
+    nodes = {}
+    nodes["out"] = Node("out")
+    for nd in nodeData:
+        id = nd[0]
+        nodes[id] = Node(id)
+
+    for nd in nodeData:
+        id = nd[0]
+        current = nodes[id]
+        for toConnect in nd[1]:
+            current.connect(nodes[toConnect])
+
+    return nodes["svr"]
+
+
+def find_unique_paths(current: Node, fft: bool, dac: bool) -> int:
+    if current.id == "out" and fft and dac:
+        return 1
+
+    total = 0
+    for child in current.connections:
+        fft = True if current.id == "fft" else fft
+        dac = True if current.id == "dac" else dac
+
+        total += find_unique_paths(child, fft, dac)
+
+    return total
+
+
+def task(data: str) -> int:
+    nodeData = parse_input(data)
+    svr = build_graph(nodeData)
+    count = find_unique_paths(svr, False, False)
+    return count
+
+
+def test_example() -> None:
+    assert task(read_file("day_11/test_p2.txt")) == 2
+
+
+def test_real() -> None:
+    result = task(get_data(day=11, year=2025))
+    print(result)
+    assert result == 571
