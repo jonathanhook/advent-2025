da88e0233e377363c274741472475d694ca351c7
Jonathan Hook <jonathan.hook@york.ac.uk>
2025-12-16 18:24:40 +0000

    Before switch to backtracking

--- day_12/d12_part_1.py
+++ day_12/d12_part_1.py
@@ -1,3 +1,5 @@
+import copy
+
 from aocd import get_data
 
 
@@ -47,10 +49,77 @@ def parse_input(data: str) -> tuple[list[Shape], list[Region]]:
     return (shapes, regions)
 
 
-def task(data: str) -> int:
-    parsed = parse_input(data)
+def check_position(p: int, s: int, r: int, region: Region, shapes: list[Shape]) -> bool:
+    # all the tiles are placed we have a solution (may need to be after placement?)
+    if sum(region.needed.values()) == 0:
+        return True
 
-    return 0
+    # if reached the end return fail
+    width = len(region.grid[0])
+    height = len(region.grid)
+    if p >= width * height:
+        return False
+
+    # skip if initial or already placed
+    col = p % width
+    row = p // width
+    if p >= 0 and s >= 0 and region.grid[row][col] == 0:
+        toPlace = shapes[s].grid
+
+        # rotate
+        for _ in range(r):
+            toPlace = [
+                [toPlace[len(toPlace) - 1 - j][i] for j in range(len(toPlace))]
+                for i in range(len(toPlace[0]))
+            ]
+
+        # check then if ok first pass, then if second pass reached place
+        for place in [False, True]:
+            for r in range(len(toPlace)):
+                for c in range(len(toPlace[0])):
+                    nr = row + r
+                    nc = col + c
+
+                    # if out of bounds fail
+                    if nr < 0 or nr >= height or nc < 0 or nc >= width:
+                        # return 0 to prune branch if placement fails?
+                        return False
+
+                    if toPlace[r][c] > 0:
+                        # if something already at this point fail
+                        if region.grid[nr][nc] > 0:
+                            # return 0 to prune branch if placement fails?
+                            return False
+
+                        if place:
+                            region.grid[nr][nc] = s
+
+        # mark a shape as placed by decrementing needed
+        region.needed[s] -= 1
+
+    # spawn the next tests
+    p += 1
+    for n in region.needed:
+        if region.needed[n] > 0:
+            for rot in range(4):
+                result = check_position(p, n, rot, copy.deepcopy(region), shapes)
+                if result:
+                    return result
+
+    # skip cell
+    result = check_position(p, -1, -1, region, shapes)
+    return result
+
+
+def task(data: str) -> int:
+    (shapes, regions) = parse_input(data)
+
+    result = 0
+    for r in regions:
+        if check_position(-1, -1, -1, r, shapes):
+            result += 1
+
+    return result
 
 
 def test_example() -> None:
