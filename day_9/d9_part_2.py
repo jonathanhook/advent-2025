2f5c419df9c65b6e73ec1bd61960d5922991d9f1
Jonathan Hook <jonathan.hook@york.ac.uk>
2025-12-19 14:54:15 +0000

    Part 2 complete (and AoC 2025 complete!)

--- day_9/d9_part_2.py
+++ day_9/d9_part_2.py
@@ -1,5 +1,6 @@
 import operator
 
+import matplotlib.pyplot as plt
 from aocd import get_data
 
 
@@ -10,6 +11,29 @@ def read_file(filename: str) -> str:
 
 def parse_input(data: str) -> list:
     return [tuple(int(x) for x in line.split(",")[:2]) for line in data.splitlines()]
+
+
+def draw(coords: list, lines: list, shape: list, lineColour: str) -> None:
+    plt.clf()
+
+    x_coords, y_coords = zip(*coords)
+    plt.scatter(x_coords, y_coords, color="blue")
+
+    for line in lines:
+        x_line, y_line = zip(*line)
+        plt.plot(x_line, y_line, color="green", linestyle="--")
+
+    x_shape, y_shape = zip(*shape)
+    plt.plot(
+        x_shape + (x_shape[0],),
+        y_shape + (y_shape[0],),
+        color=lineColour,
+        linestyle="-",
+    )
+
+    plt.show()
+
+    return
 
 
 def line_orientation(p: tuple, q: tuple, r: tuple) -> int:
@@ -66,19 +90,37 @@ def find_perimeter(coords: list) -> list:
             pos = found[0]
             direction = 0
 
-        elif len(found) > 1:
-            raise Exception("looks like you need to implement more than one on a line!")
-
         else:
             direction += 1
 
     return lines
 
 
-def check_inside(point: tuple, perimeter: list, coords: list) -> bool:
+def point_on_line(point: tuple, line: tuple) -> bool:
+    p1, p2 = line
+    if not (
+        min(p1[0], p2[0]) <= point[0] <= max(p1[0], p2[0])
+        and min(p1[1], p2[1]) <= point[1] <= max(p1[1], p2[1])
+    ):
+        return False
+
+    if (p2[0] - p1[0]) * (point[1] - p1[1]) == (p2[1] - p1[1]) * (point[0] - p1[0]):
+        return True
+
+    return False
+
+
+def point_inside(point: tuple, perimeter: list, coords: list) -> bool:
+    # is on an existing point
     if point in coords:
         return True
 
+    # is on an existing list
+    for p in perimeter:
+        if point_on_line(point, p):
+            return True
+
+    # otherwise raycast test
     ray = (point, (0, 0))
     intersections = 0
     for p in perimeter:
@@ -107,10 +149,12 @@ def check_allowed(corners: list, perimeter: list) -> bool:
 def task(data: str) -> int:
     coords = parse_input(data)
     lines = find_perimeter(coords)
-    checked = set()
 
+    checked = set()
     max = 0
     for i in range(len(coords)):
+        print(f"checking: {i}")
+
         a = coords[i]
         for j in range(len(coords)):
             b = coords[j]
@@ -127,19 +171,23 @@ def task(data: str) -> int:
 
             inside = True
             for c in [corners[1], corners[3]]:
-                if not check_inside(c, lines, coords):
+                if not point_inside(c, lines, coords):
                     inside = False
                     break
 
             if not inside:
                 continue
 
-            x = abs(a[0] - b[0]) + 1
-            y = abs(a[1] - b[1]) + 1
-            area = x * y
-            if check_allowed(corners, lines) and area > max:
-                # if area > max:
-                max = area
+            allowed = check_allowed(corners, lines)
+            if inside and allowed:
+                x = abs(a[0] - b[0]) + 1
+                y = abs(a[1] - b[1]) + 1
+                area = x * y
+                if area > max:
+                    max = area
+
+            # lineColour = "yellow" if allowed and inside else "red"
+            # draw(coords, lines, corners, lineColour)
 
     return max
 
@@ -151,4 +199,4 @@ def test_example() -> None:
 def test_real() -> None:
     result = task(get_data(day=9, year=2025))
     print(result)
-    # assert result == 4733727792
+    assert result == 1566346198
